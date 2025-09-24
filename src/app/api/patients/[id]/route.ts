2fa767484844c3511d3de444403b56ea0dc9f9c8
Misael Alves <112561288+misaelalves99@users.noreply.github.com>
2025-09-24 00:04:08 -0300

    Update route.ts

--- src/app/api/patients/[id]/route.ts
+++ src/app/api/patients/[id]/route.ts
@@ -5,8 +5,8 @@ import { Patient } from "../../../types/patient.types";
 import { initPatients, savePatients } from "../../../lib/fakePatientApi";
 
 // GET paciente por ID
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-  const id = params.id;
+export async function GET(req: NextRequest, context: any) {
+  const { id } = context.params; // sem tipagem explícita
   const patients = initPatients();
   const patient = patients.find((p) => p.id === id);
 
@@ -18,8 +18,8 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
 }
 
 // PUT: atualizar paciente
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
-  const id = params.id;
+export async function PUT(req: NextRequest, context: any) {
+  const { id } = context.params;
   const updatedData: Partial<Omit<Patient, "id" | "createdAt">> = await req.json();
   const patients = initPatients();
 
@@ -35,8 +35,8 @@ export async function PUT(req: NextRequest, { params }: { params: { id: string }
 }
 
 // DELETE: remover paciente
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
-  const id = params.id;
+export async function DELETE(req: NextRequest, context: any) {
+  const { id } = context.params;
   const patients = initPatients();
 
   const index = patients.findIndex((p) => p.id === id);
